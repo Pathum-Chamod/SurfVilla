be14d2ec98aedb295c491a39dd99343cff8b8722
pathum <spcds123@gmail.com>
2025-01-31 01:02:18 +0530

    2nd page

---
+++ assets/js/Swiper.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".singlePropertySwiper", {
+      loop: true, // Infinite loop
+      autoplay: {
+        delay: 10000, // 5 seconds interval
+        disableOnInteraction: false, // Keep autoplaying even after user interaction
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      effect: "fade", // Smooth fade transition
+    });
+  });
+  
